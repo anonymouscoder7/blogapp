f927304e3e83536ba6c45f34ce9de5fa55f8cbf9
anonymouscoder7 <lpsubedi57@gmail.com>
2024-04-10 21:01:04 +0545

    first

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/create-category',[CategoryController::class,'create']);
+Route::post('/store-category',[CategoryController::class,'store']);
+Route::get('/categories',[CategoryController::class,'index']);
+
+
